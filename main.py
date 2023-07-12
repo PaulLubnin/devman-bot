81391c6ea005f60145dafc6257c6c13875bbece6
paulego <egopaul@inbox.ru>
2023-07-12 11:08:43 +0500

    Added timestamp, work with variable token is changed.

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ from dotenv import load_dotenv
 API_DEVMAN_URL = 'https://dvmn.org/api'
 
 
-def get_server_response(auth_token, timestamp=None):
+def get_server_response(auth_token=None, timestamp=None):
     """Функция делает запросы для получения списка проверок."""
     url = f'{API_DEVMAN_URL}/long_polling/'
+    token = {'Authorization': f'Token {auth_token}'}
     payload = {'timestamp': timestamp}
-    response = requests.get(url, headers=auth_token, params=payload)
+    response = requests.get(url, headers=token, params=payload)
     response.raise_for_status()
     return response.json()
 
@@ -20,17 +21,19 @@ def get_server_response(auth_token, timestamp=None):
 def main():
     """Запуск скрипта."""
     load_dotenv()
-    devman_token = {'Authorization': f'Token {os.getenv("API_DEVMAN_TOKEN", None)}'}
+    devman_token = os.getenv('API_DEVMAN_TOKEN')
     bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
     chat_id = os.getenv('TELEGRAM_CHAT_ID')
     bot = telegram.Bot(bot_token)
 
+    request_timestamp = None
     while True:
         try:
-            jobs_check_list = get_server_response(devman_token)
+            jobs_check_list = get_server_response(devman_token, request_timestamp)
             if jobs_check_list.get('status') == 'timeout':
-                continue
+                request_timestamp = jobs_check_list.get('timestamp_to_request')
             if jobs_check_list.get('status') == 'found':
+                request_timestamp = jobs_check_list.get('last_attempt_timestamp')
                 result = jobs_check_list.get('new_attempts')[0]
                 bot.send_message(chat_id, f'У Вас проверили работу "{result["lesson_title"]}"\n'
                                           f'{result["lesson_url"]}')
